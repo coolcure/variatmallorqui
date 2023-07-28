0383517806d2345d39ce6ba0b521ada251901ab8
xisco.luque <xisco.luque@xmltravelgate.com>
2023-07-28 08:52:23 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Es Variat Mallorquí',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
